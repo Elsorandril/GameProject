c0ae206878950c85735f2448e019ed8ccec6f616
Elsorandril <rodolphe.lecomte@laposte.net>
2020-01-05 12:10:39 +0100

    Fini test move

--- Assets/Scenes/GM.cs
+++ Assets/Scenes/GM.cs
@@ -6,29 +6,28 @@ public class GM : MonoBehaviour
 {
 	private int Score;
 	private int Level;
-	private int offX;
-	private int offY;
 	private int width;
 	private int height;
 	private GameObject[,] map;
 	private GameObject[,] moveMap;
 	private Vector3 selected;
+	private LinkedList<Vector3Int> selectedMoves;
 	public int mapSizeX;
 	public int mapSizeY;
+	public Vector3 offset;
 	
 	
     void Start()
     {
         Score=0;
 		Level=1;
-		offX=3;
-		offY=3;
+		this.offset=new Vector3(3,3,0);
 		this.map=new GameObject[mapSizeX,mapSizeY];
 		this.moveMap=new GameObject[mapSizeX,mapSizeY];
 		for(int i=0;i<this.mapSizeX;i++){
 			for(int j=0;j<this.mapSizeY;j++){
 				this.map[i,j]=null;
-				this.moveMap[i,j]=Instantiate(Resources.Load<GameObject>("Prefabs/blueTintPrefab"), getCoordCenter(i, j), Quaternion.identity);
+				this.moveMap[i,j]=Instantiate(Resources.Load<GameObject>("Prefabs/blueTintPrefab"), getCoordCenter(i, j)+(new Vector3(0,0,1)), Quaternion.identity);
 			}
 		}
 		this.selected=Vector3.positiveInfinity;
@@ -48,22 +47,30 @@ public class GM : MonoBehaviour
 			//Vector2 pos = touch.position;
 			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Debug.Log("("+pos.x+","+pos.y+")");
-			Vector3 temp=positionToCell(pos);
-			if(temp.x+offX>=0&&temp.x+offX<=mapSizeX&&temp.y+offY>=0&&temp.y+offY<=mapSizeY){
+			Vector3 temp=positionToCell(pos)+this.offset;
+			if(inBound(temp)){
 				Debug.Log("Clicked");
-				Debug.Log("Temp : ("+(temp.x+offX)+","+(temp.y+offY)+")");
+				Debug.Log("Temp : ("+(temp.x)+","+(temp.y)+")");
 				if(selected.Equals(Vector3.positiveInfinity)){
-					Debug.Log("Temp Selected Only : "+this.selected);
-					
-					this.selected=temp;
+					if(!empty(temp)){
+						Debug.Log("Temp Selected Only : "+this.selected);
+						this.selected=temp;
+						this.selectedMoves=possibleMovesInRange(temp, 3);
+					}
 				}
 				else{
-					
-					Debug.Log("Temp Selected : "+this.selected);
-					Debug.Log("Temp temp: "+temp);
-					this.map[(int)temp.x+offX,(int)temp.y+offY]=this.map[(int)this.selected.x+offX,(int)this.selected.y+offY];
-					this.map[(int)this.selected.x+offX,(int)this.selected.y+offY]=null;
-					this.selected=Vector3.positiveInfinity;
+					if(!this.selected.Equals(temp)&&this.selectedMoves.Contains(Vector3Int.FloorToInt(temp))){
+						Debug.Log("Temp Selected : "+this.selected);
+						Debug.Log("Temp temp: "+temp);
+						this.map[(int)temp.x,(int)temp.y]=this.map[(int)this.selected.x,(int)this.selected.y];
+						this.map[(int)this.selected.x,(int)this.selected.y]=null;
+						this.selected=Vector3.positiveInfinity;
+						this.selectedMoves.Clear();
+					}
+					else{
+						this.selected=Vector3.positiveInfinity;
+						this.selectedMoves.Clear();
+					}
 				}
 			}
 		}
@@ -74,16 +81,26 @@ public class GM : MonoBehaviour
 	public void updateMap(){
 		for(int i=0;i<this.mapSizeX;i++){
 			for(int j=0;j<this.mapSizeY;j++){
+				this.moveMap[i,j].transform.localPosition=(getCoordCenter(i,j)+new Vector3(0,0,1));
 				if(this.map[i,j]!=null){
 					this.map[i,j].transform.localPosition =getCoordCenter(i,j);
 				}
 			}
 		}
+		if(this.selectedMoves!=null){
+			foreach(Vector3Int Vi in this.selectedMoves){
+				this.moveMap[Vi.x,Vi.y].transform.localPosition=(getCoordCenter(Vi.x,Vi.y)+new Vector3(0,0,-1));
+			}
+		}
+	}
+	
+	public bool empty(Vector3 pos){
+		return (this.map[(int)pos.x,(int)pos.y]==null);
 	}
 	
 	public Vector3 getCoordCenter(int x, int y){
 		Grid[] grid=GameObject.FindObjectsOfType( typeof(Grid) ) as Grid[];
-		return grid[0].GetCellCenterWorld(new  Vector3Int(x-offX,y-offY,0));
+		return grid[0].GetCellCenterWorld(new  Vector3Int(x,y,0)-Vector3Int.FloorToInt(this.offset));
 	}
 	
 	public Vector3 positionToCell(Vector3 pos){
@@ -96,11 +113,14 @@ public class GM : MonoBehaviour
 	public LinkedList<Vector3Int> possibleMovesInRange(Vector3 actual, int range){
 		LinkedList<Vector3Int> ll=new LinkedList<Vector3Int>();
 		for(int i=(0-range);i<range;i++){
-			for(int j=0;j<range;j++){
-				if(outOfBound(actual+(new Vector3(i,-j,0)))){
+			for(int j=0;j<(range-Mathf.Abs(i));j++){
+				Debug.Log("i: "+i+" j: "+j);
+				if(inBound(actual+(new Vector3(i,-j,0)))){
+					Debug.Log("Added i: "+i+" j: "+(-j));
 					ll.AddFirst(Vector3Int.FloorToInt(actual+(new Vector3(i,-j,0))));
 				}
-				if(outOfBound(actual+(new Vector3(i,j,0)))){
+				if(inBound(actual+(new Vector3(i,j,0)))){
+					Debug.Log("Added i: "+i+" j: "+(j));
 					ll.AddFirst(Vector3Int.FloorToInt(actual+(new Vector3(i,j,0))));
 				}
 			}
@@ -108,8 +128,9 @@ public class GM : MonoBehaviour
 		return ll;
 	}
 	
-	public bool outOfBound(Vector3 pos){
-		return (pos.x>0&&pos.x<this.mapSizeX)&&(pos.y>0&&pos.y<this.mapSizeY);
+	public bool inBound(Vector3 pos){
+		//Debug.Log("Temp3 : "+pos);
+		return (pos.x>=0&&pos.x<(this.mapSizeX))&&(pos.y>=0&&pos.y<(this.mapSizeY));
 	}
 	
 	/*public LinkedList<Coord> getDeplacemnts(Pawn[][] p){
